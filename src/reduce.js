1edaab1fe9d373d6a4b03cf51c502e158905fe1d
Ivan Dustin B. Bilon <ivan22.dust@gmail.com>
2021-06-04 08:57:28 +0800

    Improve algorithm for maximum preservation

--- src/reduce.js
+++ src/reduce.js
@@ -1,40 +1,57 @@
 const collatia = require('collatia')
 
-function reduce(selection) {
-    let entries = selection.map(flat).map(count)
-    entries     = filter_max(entries, 'words')
-    entries     = filter_max(entries, 'characters')
-    return first(entries).verse
-}
-
-function count(verse) {
-    let words      = verse.filter(identity).length
-    let characters = verse.map(word => collatia.overline.remove(word)).map(word => word.length).reduce(sum, 0)
-    return { words, characters, verse }
-}
-
-function sum(a, b) {
-    return a + b
-}
-
-function filter_max(array, property) {
-    let n   = array.map(object => object[property])
-    let max = Math.max(...n)
-    return array.filter(object => object[property] == max)
-}
-
-function first(array) {
-    return array[0]
+function reduce(selection, manuscripts) {
+    let verses = selection.map(flat)
+    let scores = verses.map(verse => score(verse, manuscripts))
+    verses     = max(verses, scores)
+    scores     = verses.map(verse => verse.filter(identity).length)
+    verses     = max(verses, scores)
+    scores     = verses.map(verse => verse.map(characters).reduce(sum, 0))
+    verses     = max(verses, scores)
+    return first(verses)
 }
 
 function identity(value) {
     return value
 }
 
+function sum(a, b) {
+    return a + b
+}
+
+function max(array, scores) {
+    let max = Math.max(...scores)
+    return array.filter((value, index)=> scores[index] == max)
+}
+
+function first(array) {
+    return array[0]
+}
+
+function get_column(index, array) {
+    return array.map(array => array[index])
+}
+
+function characters(word) {
+    return collatia.overline.remove(word).length
+}
+
 function flat(verse) {
     return verse.map(function(words) {
-        return first(filter_max(words, 'length'))
+        let scores = words.map(characters)
+        return first(max(words, scores))
     })
+}
+
+function score(verse, verses) {
+    return verse.map(function(word, index) {
+        if (word) {
+            let column = get_column(index, verses).filter(identity)
+            if (column.length > 1)
+                return 1
+        }
+        return 0
+    }).reduce(sum, 0)
 }
 
 module.exports = reduce
