a153d630b80f008702104b7d44ca74238702b663
Ivan Dustin B. Bilon <ivan22.dust@gmail.com>
2021-06-04 10:11:09 +0800

    Update test

--- test/reduce.js
+++ test/reduce.js
@@ -4,37 +4,34 @@ const collatia = require('collatia')
 
 describe('reduce', function() {
     it('is correct', function() {
-        let selection = [
-            [
-                ['A'],
-                ['B'],
-                [''],
-                ['A', 'AB', collatia.overline.add('ABC')],
-                ['C']
-            ],
-            [
-                ['A'],
-                ['B'],
-                [''],
-                ['A', 'AB', 'ABC'],
-                ['CD'],
-            ],
-            [
-                ['A'],
-                ['B'],
-                [''],
-                ['A', 'AB', 'ABC'],
-                [''],
-            ]
+        let manuscripts = [
+            ['A', 'B', 'C', 'D', 'E', '',  ''],
+            ['A', 'B', 'C', 'D', 'E', 'F', 'G']
         ]
-        let expected = [
-            'A',
-            'B',
-            '',
-            'ABC',
-            'CD'
+        let selection   = [
+            [ ['A'], ['B'], ['C'], ['D', 'DDD'],                       ['E'], [''],  ['']  ],
+            [ ['A'], ['B'], ['C'], ['D', collatia.overline.add('DD')], ['E'], [''],  ['']  ],
+            [ ['A'], ['B'], ['C'], ['D'],                              [''],  ['F'], ['G'] ],
+            [ ['A'], ['B'], [''],  ['D'],                              ['E'], [''],  ['']  ],
         ]
-        let actual = reduce(selection)
+        let expected = ['A', 'B', 'C', 'DDD', 'E', '', '']
+        let actual   = reduce(selection, manuscripts)
+        assert.deepEqual(actual, expected)
+    })
+    it('is correct', function() {
+        let manuscripts = [
+            ['A', 'B', 'C', 'D', 'E', '',  ''],
+            ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+            ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+        ]
+        let selection   = [
+            [ ['A'], ['B'], ['C'], ['D', 'DDD'],                       ['E'], [''],  ['']  ],
+            [ ['A'], ['B'], ['C'], ['D', collatia.overline.add('DD')], ['E'], [''],  ['']  ],
+            [ ['A'], ['B'], ['C'], ['D'],                              [''],  ['F'], ['G'] ],
+            [ ['A'], ['B'], [''],  ['D'],                              ['E'], [''],  ['']  ],
+        ]
+        let expected = ['A', 'B', 'C', 'D', '', 'F', 'G']
+        let actual   = reduce(selection, manuscripts)
         assert.deepEqual(actual, expected)
     })
 })
