85b79be9b79943b89afbefa526a32f4922bb4932
Ivan Dustin B. Bilon <ivan22.dust@gmail.com>
2021-06-04 15:05:12 +0800

    Update test

--- test/reduce.js
+++ test/reduce.js
@@ -4,34 +4,25 @@ const collatia = require('collatia')
 
 describe('reduce', function() {
     it('is correct', function() {
-        let manuscripts = [
-            ['A', 'B', 'C', 'D', 'E', '',  ''],
-            ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-        ]
-        let selection   = [
+        let selection = [
             [ ['A'], ['B'], ['C'], ['D', 'DDD'],                       ['E'], [''],  ['']  ],
             [ ['A'], ['B'], ['C'], ['D', collatia.overline.add('DD')], ['E'], [''],  ['']  ],
             [ ['A'], ['B'], ['C'], ['D'],                              [''],  ['F'], ['G'] ],
             [ ['A'], ['B'], [''],  ['D'],                              ['E'], [''],  ['']  ],
         ]
         let expected = ['A', 'B', 'C', 'DDD', 'E', '', '']
-        let actual   = reduce(selection, manuscripts)
+        let actual   = reduce(selection)
         assert.deepEqual(actual, expected)
     })
     it('is correct', function() {
-        let manuscripts = [
-            ['A', 'B', 'C', 'D', 'E', '',  ''],
-            ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
-            ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-        ]
-        let selection   = [
+        let selection = [
             [ ['A'], ['B'], ['C'], ['D', 'DDD'],                       ['E'], [''],  ['']  ],
             [ ['A'], ['B'], ['C'], ['D', collatia.overline.add('DD')], ['E'], [''],  ['']  ],
-            [ ['A'], ['B'], ['C'], ['D'],                              [''],  ['F'], ['G'] ],
+            [ ['A'], ['B'], ['C'], ['D'],                              ['E'], ['F'], ['G'] ],
             [ ['A'], ['B'], [''],  ['D'],                              ['E'], [''],  ['']  ],
         ]
-        let expected = ['A', 'B', 'C', 'D', '', 'F', 'G']
-        let actual   = reduce(selection, manuscripts)
+        let expected = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+        let actual   = reduce(selection)
         assert.deepEqual(actual, expected)
     })
 })
